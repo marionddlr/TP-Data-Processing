7b2825776ce9aec9acf1e84687a336d4d18be2bf
marionddlr <marion.dumasdelaroque@epfedu.fr>
2024-05-07 14:19:35 +0200

    TP3

--- data-engineering/tp-bi-spark-framework/src/main/scala/com/github/polomarcus/utils/PostgresService.scala
+++ data-engineering/tp-bi-spark-framework/src/main/scala/com/github/polomarcus/utils/PostgresService.scala
@@ -25,7 +25,13 @@ object PostgresService {
          |""".stripMargin)
 
     dataset.write
-      ???
+      .format("jdbc")
+      .option("url", url)
+      .option("dbtable", tableName)
+      .option("user", user)
+      .option("password", password)
+      .mode(SaveMode.Append)
+      .save()
 
     logger.info("Saved news inside PG database")
 
